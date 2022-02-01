09d95ed5ed2c4b0678251819a15fc399a229e0a3
Padmaj Verma <65193136+sasukexclan@users.noreply.github.com>
2022-02-01 22:08:02 +0530

    Add files via upload

---
+++ helloworld/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System;
+ 
+class Program {
+ 
+    static void Main(string[] args)
+    {
+        int[] arr = {100, 0, 5, 792};
+ 
+        try {
+ 
+            for (int i = 0; i < arr.Length; i++) {
+                Console.WriteLine(arr[i] / arr[i + 1]);
+            }
+        }
+
+        catch (IndexOutOfRangeException e) {
+ 
+            Console.WriteLine("An Exception has occurred : {0}", e.Message);
+        }
+
+        catch (DivideByZeroException e) {
+ 
+            Console.WriteLine("An Exception has occurred : {0}", e.Message);
+        }
+
+        catch (ArgumentOutOfRangeException e) {
+ 
+            Console.WriteLine("An Exception has occurred : {0}", e.Message);
+        }
+        finally {
+            for (int i = 0; i < arr.Length; i++) {
+                Console.Write(" {0}", arr[i]);
+            }
+        }
+    }
+}
